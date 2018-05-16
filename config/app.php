f45c785538ce386944ae6b465e5e8d8641f801d0
hjggvgv <souravbhattacharjeesubho1992@gmail.com>
2018-05-16 13:03:38 +0530

    facing barryvidth problem

--- config/app.php
+++ config/app.php
@@ -230,7 +230,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Image' => Intervention\Image\Facades\Image::class,
-        'PDF' => Barryvdh\DomPDF\Facade::class,
+         'PDF' => Barryvdh\DomPDF\Facade::class,
     ],
 
 ];
